61106edcc17d2cc667ef91ed94cc16415c233bd1
Daniel Samuel <dansam5000@gmail.com>
2021-05-27 16:49:13 +0100

    Refactor code and img for world quiz

---
+++ zoo_basics.sql
@@ -0,0 +1,13 @@
+-- Tutorials on SELECT Basics solutions Section 1
+--1. Modify to show the population of Germany
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+--2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark')
+
+--3. Which countries are not too small and not too big?
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
--- zoo_bbcworld.sql
+++ zoo_bbcworld.sql
@@ -1,17 +1,4 @@
--- Tutorials on SELECT Basics solutions
---1. Modify to show the population of Germany
-SELECT population FROM world
-  WHERE name = 'Germany'
-
---2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark')
-
---3. Which countries are not too small and not too big?
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
-
--- Tutorials SELECT from WORLD solutions
+-- Tutorials SELECT from WORLD solutions Section 2
 --1. SQL command to show the name, continent and population of all countries.
 SELECT name, continent, population FROM world
 
