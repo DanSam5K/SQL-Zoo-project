49b2a302eba63c9fe05d4e40f45e3bb77e602bd4
Daniel Samuel <dansam5000@gmail.com>
2021-05-28 00:35:46 +0100

    Add Select within seelect zoo sql tutorials solution 6-10

--- zoo_select.sql
+++ zoo_select.sql
@@ -25,8 +25,29 @@ SELECT name, population FROM world
 SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world
                                            WHERE name = 'Germany')), '%') FROM world
  WHERE continent = 'Europe'
---6
---7
---8
---9
---10
+--6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT name FROM world
+ WHERE gdp >= ALL(SELECT gdp FROM world 
+                   WHERE gdp >=0 AND continent = 'Europe') AND continent != 'Europe'
+
+
+--7. Find the largest country (by area) in each continent, show the continent, the name and the area:
+SELECT continent, name, area FROM world x
+ WHERE area >= ALL
+    (SELECT area FROM world y
+      WHERE y.continent=x.continent AND area>0)
+
+--8. List each continent and the name of the country that comes first alphabetically.
+SELECT continent, name FROM world x
+ WHERE name <= ALL(SELECT name FROM world y 
+                    WHERE y.continent = x.continent)
+
+--9. Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+SELECT name, continent, population FROM world x
+ WHERE 25000000  > ALL(SELECT population FROM world y 
+                         WHERE x.continent = y.continent AND y.population > 0)
+
+--10. Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+SELECT name, continent FROM world x
+ WHERE population > ALL(SELECT population*3 FROM world y 
+                         WHERE x.continent = y.continent AND population > 0 AND y.name != x.name)
